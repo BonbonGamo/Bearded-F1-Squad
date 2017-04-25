537bbc6ddac4f0f00576f1612779106a23f5bd28
Petteri Ponkamo <petteri.ponkamo@gmail.com>
2017-04-25 20:10:31 +0300

    PORT problem solved

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port: ', port)
 })
